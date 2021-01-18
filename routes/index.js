a3f78fb82f01f106f0e0a17b66ef4308f338953a
marcosullca <marco.sullca@tecsup.edu.pe>
2021-01-18 02:47:31 -0500

    hola

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const controllers = require('../controllers');
 const router = Router();
 router.get('/', (req, res) => res.send('This is root!'))
+router.get('/users', controllers.getAllUsers);
 router.post('/users', controllers.createUser);
 router.get('/users/:id', controllers.getUserById);
 router.put('/users/:id', controllers.updateUser);
